c56ad8f689b641713faa130d6cd5be2d836a16cd
Ricardo Xavier <rxc.projects@gmail.com>
2024-10-28 16:19:48 -0300

    First Commit

---
+++ InformacoesClimaticasTempoReal.java
@@ -0,0 +1,17 @@
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpRequest;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+public class InformacoesClimaticasTempoReal {
+
+    public static void main(String[] args) {
+
+    }
+
+}
